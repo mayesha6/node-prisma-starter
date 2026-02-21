0fdad741af4bb91ee1d57c79ee64aecc66bd5fa6
mayesha6 <soumyrhmn234@gmail.com>
2026-02-21 08:42:15 +0600

    auth controller

--- src/app/interfaces/index.d.ts
+++ src/app/interfaces/index.d.ts
@@ -1,7 +1,20 @@
+// declare global {
+//     namespace Express {
+//         interface Request {
+//             user: JwtPayload
+//         }
+//     }
+// }
+import { UserRole } from "@prisma/client";
+
 declare global {
-    namespace Express {
-        interface Request {
-            user: JwtPayload
-        }
+  namespace Express {
+    interface User {
+      id: string;
+      email: string;
+      role: UserRole; // better than string
     }
+  }
 }
+
+export {};
