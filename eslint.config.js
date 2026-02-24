81609be46cd0b91492cec8503ccf7344ced5b456
mayesha6 <soumyrhmn234@gmail.com>
2026-02-24 16:37:36 +0600

    CI/CD

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,15 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-useless-assignment": "off",
+      "prefer-const": "off",
+      "no-case-declarations": "off",
+    },
+  },
 ]);
